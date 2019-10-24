af59c82607733e3b0e83357431e02610578ec669
Cody Wilmeth <cwilmeth91@yahoo.com>
2019-10-24 14:05:41 -0500

    Adding Logic to convert Char Array into a String

--- CharArrayToString/src/CharArrayToString.java
+++ CharArrayToString/src/CharArrayToString.java
@@ -1,2 +1,15 @@
 public class CharArrayToString {
+
+    public static void main(String args[]){
+        char[] charArray = new char[]{'J','A','V','A',' ','I','S',' ','C','O','O','L','!'};
+
+        /*
+        To convert char array to String in Java use String(Char[] ch)
+        constructor of Java String class
+         */
+
+        String str = new String(charArray);
+
+        System.out.println("Char array converted to String: " + str);
+    }
 }
